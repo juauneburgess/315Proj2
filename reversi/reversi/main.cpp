8ac51b3db57a5d813e2dd860ea4c6d75ed68b225
juauneburgess <aeriolized@Jonathans-MacBook-Pro.local>
2013-10-12 12:11:56 -0500

    JuAune's functions functional, not fully tested for errors
    display_valid_moves, termination condition, and valid_move() completed.
    Needs further testing for full game functionality.

--- reversi/reversi/main.cpp
+++ reversi/reversi/main.cpp
@@ -7,11 +7,12 @@ int main(){
 	board.display();
 	cout<< endl;
 	board.move('e',3);
+	board.clear_possible_moves();
 	board.display();
 	board.move('d',6);
 	board.display();
 
-	char t; 
-	cin>> t;
+	//char t; 
+	//cin>> t;
 	return 0;
 }
