43080a9f74517cdeabf817e7adea6ca479c2f341
Huy Nguyen <thienhuy921@gmail.com>
2013-10-27 19:51:43 -0500

    ai files

---
+++ reversi/reversi/ai.cpp
@@ -0,0 +1,124 @@
+
+#include "ai.h"
+
+AI :: AI(){
+    srand(time(NULL));
+}
+
+vector<int> AI :: MakeMove(vector<int> possible_moves){
+    vector<int> move;
+    int rand_num = rand() % (possible_moves.size()/2);
+    cout<<rand_num << "\n";
+    move.push_back(possible_moves[rand_num*2]);
+    move.push_back(possible_moves[rand_num*2+1]);
+    cout<<move[0] << " " << move[1];
+    return move;
+}
+
+Bestmove AI :: aiMove(GameBoard currboard){
+    Bestmove choosenMove;
+    choosenMove = chooseMove(currboard, ai_color, difficulty);
+    return choosenMove;
+}
+
+Bestmove AI :: chooseMove(GameBoard currBoard, char player, int level){
+    Bestmove mybest; // my best move
+    Bestmove reply;	// opponent's best move
+    if(level == 0){ //|| currBoard.game_over()){ if level == 0 or board is full
+        if(player == ai_color){
+            mybest.score = -9999;
+            for(int i=0; i< currBoard.getValidMoves().size(); i++){
+                Move move;
+                move = currBoard.getValidMoves()[i];
+                double score = currBoard.evaluateMove(move.row,move.col);
+                if(score > mybest.score){
+                    mybest.move = move;
+                    mybest.score = score;
+                }
+            }
+        }
+        else{
+            mybest.score = 9999;
+            for(int i=0; i< currBoard.getValidMoves().size(); i++){
+                Move move;
+                move = currBoard.getValidMoves()[i];
+                double score = currBoard.evaluateMove(move.row,move.col);
+                if(score < mybest.score){
+                    mybest.move = move;
+                    mybest.score = score;
+                }
+            }
+        }
+    return mybest;
+    }
+    if(currBoard.getValidMoves().size() == 0 && player == ai_color){
+        mybest.score = 9998;
+        return mybest;
+    }
+    if(currBoard.getValidMoves().size() == 0 && player != ai_color){
+        mybest.score = -9998;
+        return mybest;
+    }
+
+    if(player == ai_color){
+        mybest.score = -9999;
+    }else{
+        mybest.score = 9999;
+    }
+    if(player == ai_color){// AI's turn
+
+        //if (currBoard.getValidMoves().size() == 0)
+        for(int i=0; i< currBoard.getValidMoves().size(); i++) //for each Move m in currBoard.possibleMoves
+        {
+            Move move;
+            move = currBoard.getValidMoves()[i];
+
+            currBoard.move(move.row, move.col); // perform move
+            reply = chooseMove(currBoard, player_color, level-1);
+            currBoard.undo(); // undo move
+            //currBoard.undo();
+            if(reply.score> mybest.score){
+                mybest.move = move;
+                mybest.score = reply.score;
+            }
+        }
+    }else{// optimal human's turn
+        for(int i=0; i< currBoard.getValidMoves().size(); i++)
+        {
+            Move move;
+            move = currBoard.getValidMoves()[i];
+
+            currBoard.move(move.row, move.col);// perform move
+            reply = chooseMove(currBoard, ai_color, level-1);
+            currBoard.undo(); // undo move
+            //currBoard.undo();
+            if(reply.score < mybest.score){
+                mybest.move = move;
+                mybest.score = reply.score;
+            }
+        }
+    }
+
+    return mybest;
+
+
+}
+
+
+Bestmove AI :: move(GameBoard currBoard){
+    Bestmove mybest;
+    cout<<"\n";
+    mybest.score = -9999;
+    for(int i=0; i< currBoard.getValidMoves().size(); i++){
+        Move move;
+        move = currBoard.getValidMoves()[i];
+        double score = currBoard.evaluateMove(move.row,move.col);
+        if(score > mybest.score){
+            mybest.move = move;
+            mybest.score = score;
+        }
+    }
+
+
+    return mybest;
+}
---
+++ reversi/reversi/ai.h
@@ -0,0 +1,47 @@
+#ifndef AI_H
+#define AI_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include "gameboard.h"
+
+using namespace std;
+
+class Bestmove{
+public:
+    Move move;
+    double score;
+    Bestmove(){
+        move.row = 0;
+        move.col = 0;
+        score = 0;
+    }
+
+};
+
+class AI{
+private:
+
+    int difficulty;
+    char ai_color;
+    char player_color;
+
+public:
+
+    AI();
+
+    void set_difficulty(int diff){difficulty = diff;}
+    void set_ai_color(char _aicolor, char _playercolor){ai_color = _aicolor; player_color = _playercolor;}
+    vector<int> MakeMove(vector<int> possible_moves);
+    //Bestmove chooseMove(GameBoard, bool player, int level);
+    Bestmove aiMove(GameBoard);
+    Bestmove chooseMove(GameBoard, char player, int level);
+    Bestmove move(GameBoard);
+
+};
+
+#endif // AI_H
